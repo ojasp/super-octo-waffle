5515340ff7164528ecf0bf8967a99e42e6acd4aa
ojasp <opanwar@gmail.com>
2016-04-26 11:59:34 -0400

    Update Add conditions to url rewrite rule.ps1

--- Add conditions to url rewrite rule.ps1
+++ Add conditions to url rewrite rule.ps1
@@ -7,23 +7,25 @@ $RuleName = 'RequestBlockingRule1 with spaces'
 $listofcollections = Get-WebConfigurationProperty -PSPath "MACHINE/WEBROOT/APPHOST/$Websitename" -Filter "/system.webServer/rewrite/rules/rule[@name='$RuleName']/conditions" -Name collection | select -ExpandProperty pattern
 
 ForEach($URI in $blockedURIs){
-$list = @{
- pspath = "MACHINE/WEBROOT/APPHOST/$Websitename"
- filter = "/system.webServer/rewrite/rules/rule[@name='$RuleName']/conditions"
- Value = @{
-    input = '{REQUEST_URI}'
-    matchType ='Pattern'
-    pattern =$URI
-    ignoreCase ='True'
-    negate ='False'
-}
-}
 
 ####### Check to see if that condition already exists in $RuleName
 if ($URI -notin $listofcollections) {
-    Add-WebConfiguration @list
+    $list = @{
+     pspath = "MACHINE/WEBROOT/APPHOST/$Websitename"
+     filter = "/system.webServer/rewrite/rules/rule[@name='$RuleName']/conditions"
+     Value = @{
+        input = '{REQUEST_URI}'
+        matchType ='Pattern'
+        pattern =$URI
+        ignoreCase ='True'
+        negate ='False'
+        }
     }
+    Write-Output "[+] Adding '$URI' to '$RuleName'"
+    Add-WebConfiguration @list
+}
+
 else {
-    Write-Output "$URI is already exists as a condition."
+    Write-Output "[!] '$URI' already exists as a condition in '$RuleName'"
     }
 }
